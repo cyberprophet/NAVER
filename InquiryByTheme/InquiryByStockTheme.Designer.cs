6fb7ac40103f7fedc95b9fa595cc58221658a588
SANGWOO PARK <cyberprophet@hanmail.net>
2023-12-12 21:21:22 +0900

    automate inquiry by theme

---
+++ InquiryByTheme/InquiryByStockTheme.Designer.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel;
+
+namespace ShareInvest;
+
+partial class InquiryByStockTheme
+{
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            if (timer is not null)
+            {
+                timer.Stop();
+                timer.Tick -= TimerTick;
+                timer.Dispose();
+            }
+            if (notifyIcon is not null)
+            {
+                if (notifyIcon.Icon is not null)
+                {
+                    notifyIcon.Icon.Dispose();
+                }
+                notifyIcon.Dispose();
+            }
+            if (Controls.Count > 0)
+            {
+                foreach (Control control in Controls)
+                {
+                    control.Dispose();
+                }
+                Controls.Clear();
+            }
+            if (components != null)
+            {
+                components.Dispose();
+
+                components = null;
+            }
+        }
+        base.Dispose(disposing);
+    }
+    void InitializeComponent()
+    {
+        components = new Container();
+        ComponentResourceManager resources = new ComponentResourceManager(typeof(InquiryByStockTheme));
+        notifyIcon = new NotifyIcon(components);
+        timer = new System.Windows.Forms.Timer(components);
+        SuspendLayout();
+        // 
+        // notifyIcon
+        // 
+        notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+        notifyIcon.Icon = Properties.Resources.DARK;
+        notifyIcon.Text = "AnTalk";
+        // 
+        // timer
+        // 
+        timer.Interval = 1000;
+        timer.Tick += TimerTick;
+        // 
+        // InquiryByStockTheme
+        // 
+        AutoScaleDimensions = new SizeF(96F, 96F);
+        AutoScaleMode = AutoScaleMode.Dpi;
+        BackColor = Color.Black;
+        ClientSize = new Size(800, 450);
+        DoubleBuffered = true;
+        Icon = (Icon)resources.GetObject("$this.Icon");
+        Name = "InquiryByStockTheme";
+        ShowInTaskbar = false;
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "동학개미운동";
+        FormClosing += JustBeforeFormClosing;
+        Resize += SecuritiesResize;
+        ResumeLayout(false);
+    }
+
+    ComponentResourceManager resources;
+    NotifyIcon notifyIcon;
+    System.Windows.Forms.Timer timer;
+    System.ComponentModel.IContainer components;
+}
---
+++ InquiryByTheme/Program.cs
@@ -0,0 +1,18 @@
+using ShareInvest.Properties;
+
+namespace ShareInvest;
+
+static class Program
+{
+    [STAThread]
+    static void Main(string[] arg)
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new InquiryByStockTheme(arg[0],
+        [
+            Resources.GREEN,
+            Resources.WHITE,
+            Resources.DARK
+        ]));
+    }
+}
