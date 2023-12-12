5a8932985e87adb87ee77bbbfb07f696b6cd1631
SANGWOO PARK <cyberprophet@hanmail.net>
2023-12-12 12:28:09 +0900

    초기 커밋.

---
+++ Theme/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.Chrome;
+
+using (var service = ChromeDriverService.CreateDefaultService())
+{
+    var options = new ChromeOptions
+    {
+
+    };
+#if DEBUG
+
+#else
+    options.AddArgument("--headless");
+#endif
+    options.AddArgument("--window-size=1920,1080");
+
+    var driver = new ChromeDriver(service, options);
+
+    driver.Navigate().GoToUrl("https://finance.naver.com/sise/theme.naver");
+
+    driver.Quit();
+}
