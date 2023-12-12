c236c6b147358928b44e868937a955074f09c694
SANGWOO PARK <cyberprophet@hanmail.net>
2023-12-12 15:09:15 +0900

    collection by theme

---
+++ Theme/ThemeModel.cs
@@ -0,0 +1,41 @@
+﻿namespace ShareInvest;
+
+class ThemeModel
+{
+    internal string? ThemeName
+    {
+        get; set;
+    }
+    internal string? ThemeCode
+    {
+        get; set;
+    }
+    internal string? RateCompareToPreviousDay
+    {
+        get; set;
+    }
+    internal string? AverageRateLast3Days
+    {
+        get; set;
+    }
+    internal string? RisingStockCount
+    {
+        get; set;
+    }
+    internal string? FlatStockCount
+    {
+        get; set;
+    }
+    internal string? FallingStockCount
+    {
+        get; set;
+    }
+    internal string? FirstLeadingStockCode
+    {
+        get; set;
+    }
+    internal string? SecondLeadingStockCode
+    {
+        get; set;
+    }
+}
